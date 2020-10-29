0efecad1dc2224ddfe0b204c09521d888653604c
Taha Galata <tgalata2000@gmail.com>
2020-10-29 22:32:23 +0300

    ready to make requests

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
 import {playlistUrlToVideoDetailsArray as youtube} from './youtubeFunctions.js'
 import express from 'express'
 
-
 const app = express()
 const port = 3000
 
-const playlist = "PLRfY4Rc-GWzhdCvSPR7aTV0PJjjiSAGMs"
 
+app.use(express.json());
 
-youtube(playlist).then(console.log)
+app.post('/', (req, res) => {
+  youtube(req.body.url).then(videoList => res.json(videoList))
+})
 
-// app.use(express.urlencoded({extended: false}));
-// app.use(express.json());
-
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.post('/', (req, res) => {
-//   res.send(req.body)
-// })
-
-// app.listen(port, () => {
-//   console.log(`App listening at http://localhost:${port}`)
-// })
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
