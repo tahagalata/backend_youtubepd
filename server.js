1eefc391d9e945c79979aa8f7c74b3ec260e964b
Taha Galata <tgalata2000@gmail.com>
2020-11-08 14:12:19 +0300

    added cors

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 import {playlistUrlToVideoDetailsArray as youtube} from './youtubeFunctions.js'
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
-const port = 3000
+const port = 3001
 
-
+app.use(cors())
 app.use(express.json());
 
+
 app.post('/', (req, res) => {
-  youtube(req.body.url).then(videoList => res.json(videoList))
+  // youtube(req.body.url).then(videoList => res.json(videoList))
+  console.log(req.body)
+  res.json(req.body)
 })
 
 app.listen(port, () => {
